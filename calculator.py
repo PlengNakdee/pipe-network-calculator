83335756e5f911e9172e6333f62e634b77c36be5
PlengNakdee <plengwn@gmail.com>
2023-03-10 03:12:11 +0000

    checkpoint

--- calculator.py
+++ calculator.py
@@ -2,8 +2,6 @@ import math
 import json
 
 epsi = 0.000001
-# writeln(Liqmax:6)
-# writeln(Value : field_width)
 
 def cal_decf(value):
     if abs(value) > epsi:
@@ -22,11 +20,33 @@ def cal_decf(value):
 def read_file(file_name):
     with open(file_name) as f:
         file_contents = f.read()
-        print('file-contents', file_contents)
+        # print('file-contents', file_contents)
         return file_contents
 
-def cal_punp_hp(pr, q):
+def load_file(file_name):
+    with open(file_name) as f:
+        file_contents = json.load(f)
+        # print('file-contents', file_contents)
+        return file_contents
 
+def cal_bolier():
+    boiler_data = load_file('./dummy/boiler.json')
+    
+    for key, value in boiler_data.items():
+        print('value name', value['name'])
+
+def cal_reynolds(mFlow, D, T, P):
+    # mFlow : Mass flow                     [kg/s]
+    # D     : Inner diameter of the pipe    [mm]
+    # T     : Temperature of the water      [°C]
+    # P     : Hydraulic Static Pressure     [bar]
+
+    pi_number = 4 * math.atan(1)
+    reynolds = 4 * mFlow / pi_number * (D/1000) * P
+    print('reynolds: ', reynolds)
+    return reynolds
 
 # cal_decf(9)
 # read_file('./dummy/liquid.json')
+# cal_bolier()
+cal_reynolds(100, 20, 140, 2)
